309d5afe3ea6f2734ab739aa9f9ee4254f4dcb0c
Sattar Hummatli <settarxan@gmail.com>
2017-06-14 14:33:29 +0400

    Learn Variables

---
+++ KotlinProgramming/src/Main.kt
@@ -0,0 +1,19 @@
+/**
+ * Created by settar on 6/14/17.
+ */
+
+
+fun main(args: Array<String>) {
+    println("Hello World Kotlin from Sattar")
+
+    var udemy = "Udemy"
+
+    println(udemy)
+
+    val pi = 3.14
+
+    //pi = 3 // can not be reassagned
+
+    println(pi)
+
+}
