afe94f419ca6144ff0d1dc74ab99afdf6bb7bee5
Sattar Hummatli <settarxan@gmail.com>
2017-06-14 20:13:15 +0400

    Tryed Kotlin Arrays

---
+++ KotlinProgramming/src/collections/ArraysLists.kt
@@ -0,0 +1,38 @@
+package collections
+
+/**
+ * Created by settar on 6/14/17.
+ */
+
+fun main(args: Array<String>) {
+
+    //Arrays----------------------------------------
+    println("Arrays --------------------")
+    val array = arrayOf("Texas", "NewYork", "Colifornia")
+    val mixed = arrayOf("Kotlin", 17, false, 5.9)
+    val numbers = intArrayOf(18, 29, 22)
+    println(mixed[0])
+
+    val myName = "Sattar"
+    println("My name's third letter is " + myName[2])
+
+    val states2 = arrayOf("Nevada", "Iowa")
+    val allStates = array + states2
+    println("All states size = " + allStates.size)
+
+    val empty = mixed.isEmpty()
+    println("Mixed is $empty")
+
+    if (allStates.contains("Arizona")){
+        println("Contains Arizona")
+    }else{
+        println("Does not contain Arizona")
+    }
+
+
+
+    val list = listOf("Apple", "Nar", "Heyva")
+
+    val arrayList = arrayListOf("Ali", "Nargiz", "Sattar")
+
+}
