c9907b9558aa6453e496546f7987097f23c4076c
Sattar Hummatli <settarxan@gmail.com>
2017-06-15 16:08:44 +0400

    Created Class challenge program

--- KotlinProgramming/src/challenges/Class-Challenge.kt
+++ KotlinProgramming/src/challenges/Class-Challenge.kt
@@ -2,4 +2,30 @@ package challenges
 
 /**
  * Created by settar on 6/15/17.
+ * Class challenge created by me in Kotlin course
  */
+
+class Book {
+    var title: String = "Rich Dad Poor Dad"
+    var author: String = "Kiyosaki"
+    var publicatioYear = 1990
+    var borrowed: Boolean = false
+
+    fun borrowedFromMe(){
+        borrowed = true
+    }
+
+    fun returnedToMe(){
+        borrowed = false
+    }
+
+    fun print(){
+        println("Title: $title, Author: $author, Publication year: $publicatioYear ")
+    }
+}
+
+fun main(args: Array<String>) {
+    val book = Book()
+    book.print()
+}
+
