fd06810fe632ef70baec6bc6b1328d34ce560105
Sattar Hummatli <settarxan@gmail.com>
2017-06-16 15:02:31 +0400

    Added properties and methods to Color enum

--- KotlinProgramming/src/oo/Color.kt
+++ KotlinProgramming/src/oo/Color.kt
@@ -4,6 +4,15 @@ package oo
  * Created by settar on 6/16/17.
  */
 
-enum class Color{
-    RED, GREEN, BLUE
+enum class Color(val rgb: Int){
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+
+    fun print(){
+        println("This my RGB number $rgb")
+    }
+}
+
+
+fun main(args: Array<String>) {
+    Color.RED.print()
 }
